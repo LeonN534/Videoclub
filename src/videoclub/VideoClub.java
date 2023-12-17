b7d95c0d24a03bd795652dd8c9d0041430332791
Leonardo <leo321war@gmail.com>
2023-12-17 16:55:17 -0500

    Uploaded project to GitHub

---
+++ src/videoclub/VideoClub.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package videoclub;
+
+public class VideoClub {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static final String URL = "";
+    public static final String USERNAME = "";
+    public static final String PASSWORD = "";
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Menu ventanaPrincipal = new Menu();
+        ventanaPrincipal.setLocationRelativeTo(null);
+        ventanaPrincipal.setVisible(true);
+    }
+    
+}
